cbf57f1de8039f4386f3294b8aeceff94a6d4d4d
unknown <anstryna46@gmail.com>
2025-10-06 19:11:44 +0700

    Mengerjakan Latihan Day3

--- OOP-Inheritance-Challenge/day03/template.py
+++ OOP-Inheritance-Challenge/day03/template.py
@@ -1,8 +1,20 @@
 class Pegawai:
-    pass
+    def __init__(self, nama, gaji_dasar):
+        self.nama = nama
+        self.gaji_dasar = gaji_dasar 
+    def hitungGaji(self):
+        return self.gaji_dasar
 
 class Manager(Pegawai):
-    pass
+    def __init__(self, nama, gaji_dasar, tunjangan):
+        super().__init__(nama, gaji_dasar)
+        self.tunjangan = tunjangan
 
+    def hitungGaji(self):
+        gaji_dasar = super().hitungGaji()
+        print(f"Gaji {self.nama} adalah {gaji_dasar + self.tunjangan}")
+
+org1 = Manager("Andi", 3700000, 550000)
+org1.hitungGaji()
 if __name__ == "__main__":
     pass
