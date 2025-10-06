b6f9c3848d44e3a77fda6e4abd1958d06cb2a316
unknown <anstryna46@gmail.com>
2025-10-06 19:04:08 +0700

    Mengerjakan latihan Day2

--- OOP-Inheritance-Challenge/day02/template.py
+++ OOP-Inheritance-Challenge/day02/template.py
@@ -1,11 +1,26 @@
 class Kendaraan:
-    pass
+    def __init__(self, merk, kecepatan_maksimal):
+        self.merk = merk
+        self.kecepatan_maksimal = kecepatan_maksimal
 
 class Mobil(Kendaraan):
-    pass
+    def __init__(self, merk, kecepatan_maksimal, jumlah_kursi):
+        super().__init__(merk, kecepatan_maksimal)
+        self.jumlah_kursi = jumlah_kursi
+    def berjalan(self):
+        print(f"mobil {self.merk} dengan jumlah kursi {self.jumlah_kursi} bisa berjalan dengan kecepatan maksimal {self.kecepatan_maksimal} km/jam")
 
 class Motor(Kendaraan):
-    pass
+    def __init__(self, merk, kecepatan_maksimal, tipe_motor):
+        super().__init__(merk, kecepatan_maksimal)
+        self.tipe_motor = tipe_motor
+    def berjalan(self):
+        print(f"motor {self.merk} dengan tipe {self.tipe_motor} bisa berjalan dengan kecepatan maksimal {self.kecepatan_maksimal} km/jam")
+
+mtr1 = Motor("Honda", 150, "Matic")
+mtr1.berjalan()
+mbl1 = Mobil("Toyota", 180, 8)
+mbl1.berjalan()
 
 if __name__ == "__main__":
     pass
