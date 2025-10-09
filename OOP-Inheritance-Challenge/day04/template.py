e2bff75a2d4f581357b8530d611290b690506896
unknown <anstryna46@gmail.com>
2025-10-09 22:20:57 +0700

    mengerjakan latihan day 4 dadn 5

--- OOP-Inheritance-Challenge/day04/template.py
+++ OOP-Inheritance-Challenge/day04/template.py
@@ -1,11 +1,25 @@
 class LivingBeing:
-    pass
+    def __init__(self, jenis, habitat):
+        self.jenis = jenis
+        self.habitat = habitat
+    def bernapas(self):
+        print(f"{self.jenis} bisa bernapas")
 
 class Animal(LivingBeing):
-    pass
+    def __init__(self, jenis, habitat, nama):
+        super().__init__(jenis, habitat)
+        self.nama = nama
+    def bergerak(self):
+        print(f"{self.nama} bisa bergerak ")
 
 class Bird(Animal):
-    pass
+    def __init__(self, jenis, habitat, nama, warna):
+        super().__init__(jenis, habitat, nama)
+        self.warna = warna
+    def terbang(self):
+        print(f"Burung {self.nama} dengan warna {self.warna} bisa terbang")
 
+lv = Bird("Burung", "Hutan", "LoveBird", "Hijau")
+lv.terbang()
 if __name__ == "__main__":
     pass
--- OOP-Inheritance-Challenge/day05/template.py
+++ OOP-Inheritance-Challenge/day05/template.py
@@ -1,11 +1,38 @@
 class BankAccount:
-    pass
+    def __init__(self, namaPemilik, namaBank, saldo):
+        self.namaPemilik = namaPemilik
+        self.namaBank = namaBank
+        self.saldo = saldo
+    
+    def deposit(self, jumlah):
+        self.saldo += jumlah
+        return self.saldo
 
 class SavingsAccount(BankAccount):
-    pass
+    def __init__(self, namaPemilik, namaBank, saldo, bunga=0.02):
+        super().__init__(namaPemilik, namaBank, saldo)
+        self.bunga = bunga
+    def tambahBunga(self):
+        totalBunga = self.saldo * self.bunga
+        self.saldo += totalBunga
+        print(f"{self.namaPemilik} mendapatkan bunga sebesar : {totalBunga} %, saldo akhir : {self.saldo}")
 
 class CheckingAccount(BankAccount):
-    pass
+    def __init__(self, namaPemilik, namaBank, saldo, biayaAdmin=3000):
+        super().__init__(namaPemilik, namaBank, saldo)
+        self.biayaAdmin = biayaAdmin
+    def potongBiayaAdmin(self):
+        self.saldo -= self.biayaAdmin
+        print(f"{self.namaPemilik} dikenakan biaya admin sebesar : {self.biayaAdmin}. sisa saldo : {self.saldo}")
+
+triya = SavingsAccount("Triya", "Mandiri", 1750000)
+anis = CheckingAccount("Anis", "Mandiri", 2500000)
+triya.deposit(500000)
+triya.tambahBunga()
+anis.deposit(1300000)
+anis.potongBiayaAdmin()
+
+
 
 if __name__ == "__main__":
     pass
